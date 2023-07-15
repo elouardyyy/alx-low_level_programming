569329776ac0e489690d72f3155050ee0b52e83c
Wissal El Ouardy <wissal.elouardi12@gmail.com>
2023-07-14 19:38:38 -0700

    c variables if else while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints numbers from 0 to 9.
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int num;
-
-	for (num - 0; num < 10; num++)
-		putchar((num % 10) + '0');
-
+	int i;
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	}
 	putchar('\n');
-
 	return (0);
 }
 
